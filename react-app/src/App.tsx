eecba6fc716833d0b5a37bfefd06a02350b344ef
Howard Lo <warhod@users.noreply.github.com>
2023-11-05 18:26:59 -0800

    Glitchy effect

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -13,7 +13,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <div className="App-header">        
-        <h1 className='glow '>AI 発生器</h1>
+        <h1 className='glow glitch layers'><span>AI 発生器</span></h1>
         <button className="buttonPosition" onClick={getRandomQuote}>Tease Me!</button>
         {randomQuote && <p>{randomQuote}</p>}
         <div className="environment"></div>
