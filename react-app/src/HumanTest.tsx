76630784e7b67fcc954d690b575a7173fc128687
Howard Lo <warhod@users.noreply.github.com>
2023-11-09 00:59:20 -0800

    Fixing all the test logic

--- react-app/src/HumanTest.tsx
+++ react-app/src/HumanTest.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import humantest from './humantest.json'
 import './HumanTest.css'
 
@@ -11,58 +11,77 @@ type Test = {
 
 const HumanTest: React.FC = () => {
   
-  const [currentHumanTest, setCurrentHumanTest] = useState<Test>({id: 0, question: "", images: [], correctAnswer: 0});
+  const [currentHumanTest, setCurrentHumanTest] = useState<Test>({
+    id: -1, 
+    question: "", 
+    images: [], 
+    correctAnswer: -1
+  });
+  const [glowingImage, setGlowingImage] = useState<number>(-1); // -1 means no answer is selected
+  const [selectedAnswer, setSelectedAnswer] = useState<number>(-1); 
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState<boolean>(false);
+  const [isDisplayTest, setIsDisplayTest] = useState<boolean>(true);
+  
+  useEffect(() => {
+    // Load the initial question and images when the component mounts
+    getRandomHumanTest();
+  }, []); // Empty dependency array means this effect runs once on component mount
+
   const getRandomHumanTest = () => {
-    
     const randomIndex = Math.floor(Math.random() * humantest.length);
-    console.log("randomIndex:" + randomIndex);
-    console.log("Fetched test:" + humantest[randomIndex]);
     setCurrentHumanTest(humantest[randomIndex]);
+    setGlowingImage(-1); // Clear the glowing effect on new question
+    setSelectedAnswer(-1); // Clear the selected answer
+    setIsCorrectAnswer(false); // Clear the correctness status
+    setIsDisplayTest(true); // display another test
   };
 
-  const [glowingImage, setGlowingImage] = useState<number>(-1); // -1 means no answer is selected initially
-  const [selectedAnswer, setSelectedAnswer] = useState<number>(0); // 0 means no answer is selected.
-  const [isCorrectAnswer, setIsCorrectAnswer] = useState<boolean>(false);
-  
-  const getNextQuestion = () => {
-    setIsCorrectAnswer(false);
-    getRandomHumanTest();
-  };
+  const checkIsCorrectAnswer = () => {
+    setIsCorrectAnswer(selectedAnswer === currentHumanTest.correctAnswer);
+  }
 
   return (
     <div>
-      <h1>CAPTCHAI TEST</h1>
+      <h1 className='glow glitch layers'>CAPTCHAI TEST</h1>
       <div>
-        <h1>{currentHumanTest.question}</h1>
-        <div className="image-container">
-          {currentHumanTest.images.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt={`Image ${index + 1}`}
-              onClick={() => {
-                setSelectedAnswer(index);
-                setGlowingImage(index);     // Glows image to indicate selection
-              }}
-              className={`glow-image ${glowingImage === index ? 'glowing' : ''}`}
-            />
-          ))}
-        </div>
-        <button onClick={getNextQuestion}>Verify I'm Human</button>
-        <div>
-          {isCorrectAnswer === null ? null : (
-            <div>
-              {selectedAnswer === currentHumanTest.correctAnswer ? (
-                <p>Correct! Welcome human!</p>
-              ) : (
-                <div>
-                  <p>Wrong! Try Again.</p>
-                  <button onClick={getNextQuestion}>Try Again</button>
-                </div>
-              )}
+        {isDisplayTest ? (
+          <div>
+            <div>{currentHumanTest.question}</div>
+            <div className="image-container">
+              {currentHumanTest.images.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  alt={`Image ${index + 1}`}
+                  onClick={() => {
+                    setSelectedAnswer(index);
+                    setGlowingImage(index);     // Glows image to indicate selection
+                  }}
+                  className={`glow-image ${glowingImage === index ? 'glowing' : ''}`}
+                />
+              ))}
             </div>
-          )}
-        </div>
+            <button onClick={() => {
+                checkIsCorrectAnswer();
+                setIsDisplayTest(false)
+              }}>Verify I'm Human</button>
+          </div>
+        ) : (  
+          <div>
+            {isCorrectAnswer === null ? null : (
+              <div>
+                {isCorrectAnswer ? (
+                  <p>Correct! Welcome human!</p>
+                ) : (
+                  <div>
+                    <p>Wrong! Try Again.</p>
+                    <button onClick={getRandomHumanTest}>Try Again</button>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
